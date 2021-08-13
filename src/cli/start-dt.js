5378d750f46d983bbe0c4c7f9af1b4865b8df862
xkool <shk@xkool.org>
2021-08-13 19:43:57 +0800

    fix: re

--- src/cli/start-dt.js
+++ src/cli/start-dt.js
@@ -39,7 +39,7 @@ export async function startDtAction() {
       break;
   }
 
-  const dtWebDir = path.join(baseDir, "packages/dt-web");
+  const dtWebDir = path.join(baseDir, "./packages/dt-web");
   if (!fs.existsSync(dtWebDir)) {
     await git.clone(["git@git.xkool.org:xkool_plan/dt_frontend.git"]);
   }
@@ -49,13 +49,12 @@ export async function startDtAction() {
   const packages = ["api", "dt", "graphic", "ui", "utils"];
 
   let dtWebDirJson = require(path.join(dtWebDir, "package.json"));
-  //   const baseDirJson = require(path.join(baseDir, "package.json"));
-  console.log(dtWebDirJson, "dtWebDirJson 11");
+
   let hasChanged = false;
   for await (let packageName of packages) {
     const packageVersion = path.join(
       baseDir,
-      `packages/${packageName}/package.json`
+      `./packages/${packageName}/package.json`
     );
     if (dtWebDirJson.dependencies[`@xkool/${packageName}`] !== packageVersion) {
       if (!hasChanged) {
@@ -70,15 +69,15 @@ export async function startDtAction() {
       };
     }
   }
-  console.log(dtWebDirJson, "dtWebDirJson 22");
 
+  console.log(dtWebDir, "dtWebDir 11");
+  console.log(hasChanged, path.join(dtWebDir, "package.json"), "dtWebDir 22");
   if (hasChanged) {
     fs.writeFileSync(
       path.join(dtWebDir, "package.json"),
       JSON.stringify(dtWebDirJson, null, 2)
     );
   }
-
   const baseDirLernaJson = require(path.join(baseDir, "lerna.json"));
   if (
     !baseDirLernaJson.packages.find(
@@ -86,8 +85,9 @@ export async function startDtAction() {
     ) &&
     baseDirLernaJson.packages[0] !== "packages/*"
   ) {
+    console.log("write!!!!");
     fs.writeFileSync(
-      path.join(baseDir, "lerna.json"),
+      path.join(baseDir, "./lerna.json"),
       JSON.stringify(
         {
           ...baseDirLernaJson,
@@ -102,7 +102,7 @@ export async function startDtAction() {
   await shellJsAsync("lerna bootstrap");
 
   for await (let packageName of packages) {
-    const pDir = path.join(baseDir, `packages/${packageName}`);
+    // const pDir = path.join(baseDir, `packages/${packageName}`);
     await shellJsAsync(`cd packages/${packageName} && npm run build`);
   }
 
