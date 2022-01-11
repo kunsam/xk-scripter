753dfbf05fcd15e81731e03e1874ae7a661478a2
xkool <shk@xkool.org>
2022-01-11 15:59:58 +0800

    feat: new version

--- src/cli/core.js
+++ src/cli/core.js
@@ -9,6 +9,7 @@ import chalk from "chalk";
 import { gcb, gcba, gco, gcoa, gsave, guse, gclear } from "xks-git";
 import shell from "shelljs";
 import { startDtAction } from "./start-dt";
+import { DTDOC_LINK } from "../dtdoc.config";
 
 const program = new Command();
 
@@ -184,14 +185,53 @@ program
   });
 
 program
-  .command("document_link")
-  .description("show doc link")
+  .command("helpdoc")
+  .description("show xksciprt doc")
   .action(() => {
     console.log(
       chalk.green(
         "https://www.notion.so/kunsam624/xk-scripter-25dfe18afc854797bde47024a43fedbb"
       )
     );
+  });
+
+function recursiveGetResult(clist) {
+  clist.forEach((target, index) => {
+    console.log(chalk.white(`${index + 1}. ${target}`));
+  });
+  const chosenIndex = loopInput("请选择：", (input) => {
+    const choose = input && parseInt(input);
+    if (choose && choose > 0 && choose <= clist.length) return choose - 1;
+  });
+  if (clist[chosenIndex]) {
+    if (clist[chosenIndex].link) {
+      chalk.green(clist[chosenIndex].link);
+    } else if (clist[chosenIndex].children) {
+      recursiveGetResult(clist[chosenIndex].children);
+    }
+  }
+}
+
+program
+  .command("dtdoc")
+  .description("show dt develop doc")
+  .action(() => {
+    console.log(chalk.magenta(`\n-- [文档列表]:`));
+    DTDOC_LINK.forEach((target, index) => {
+      console.log(chalk.white(`${index + 1}. ${target}`));
+    });
+    const chosenIndex = loopInput("请选择：", (input) => {
+      const choose = input && parseInt(input);
+      if (choose && choose > 0 && choose <= DTDOC_LINK.length)
+        return choose - 1;
+    });
+    if (DTDOC_LINK[chosenIndex]) {
+      if (DTDOC_LINK[chosenIndex].link) {
+        chalk.green(DTDOC_LINK[chosenIndex].link);
+      } else if (DTDOC_LINK[chosenIndex].children) {
+        recursiveGetResult(DTDOC_LINK[chosenIndex].children);
+      }
+    }
   });
 
 program
---
+++ src/dtdoc.config.js
@@ -0,0 +1,27 @@
+export const DTDOC_LINK = [
+  {
+    name: "单体技术开发文档(总入口）",
+    link: "https://wiki.xkool.org/pages/viewpage.action?pageId=100534143",
+  },
+  {
+    name: "数学几何文档(基础文档）",
+    link: "https://wiki.xkool.org/pages/viewpage.action?pageId=100534159",
+  },
+  {
+    name: "Jsts专题文档(通用功能文档）",
+    link: "https://wiki.xkool.org/pages/viewpage.action?pageId=100534378",
+  },
+  {
+    name: "对象几何计算文档(通用功能文档）",
+    link: "https://wiki.xkool.org/pages/viewpage.action?pageId=100534189",
+  },
+  {
+    name: "业务文档",
+    children: [
+      {
+        name: "标注业务",
+        link: "https://wiki.xkool.org/pages/viewpage.action?pageId=100534388",
+      },
+    ],
+  },
+];
